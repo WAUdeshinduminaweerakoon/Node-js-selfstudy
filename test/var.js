f67c279363be25cefe41fc8b0da7b167931c3a99
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-08-15 20:45:12 +0530

    variable

---
+++ test/var.js
@@ -0,0 +1,13 @@
+let firstName = "Udesh"
+let a = "10";
+let b = "20";
+
+console.log(a+b);// 1020 string variable 
+let c = 5;
+let d = 6;
+
+console.log(c+d);//11 integer variable
+console.log(5-6)
+
+let x = true;
+
