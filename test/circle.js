f87eca14cdf966614f7765cbe66b0e1c7b41508c
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-08-15 21:39:08 +0530

    calculation

---
+++ test/circle.js
@@ -0,0 +1,21 @@
+/* 
+Find the diameter, area and circumference of radius 20cm.
+Diameter pf a Circale D= 21*r
+Circumference of a Circle C=2*PI*r
+Area of a Circle  A= PI*r*r
+PI = 3.141
+*/
+let radius = 20;
+const PI = Math.PI;
+let diameter;
+diameter =2*radius;
+let circumference = 2*PI*radius;
+let area =PI*radius**2;
+
+console.log("Diameter :- "+diameter.toFixed(3));
+console.log("Circumference :- " +circumference.toFixed(3));
+console.log("Area :- " +area.toFixed(3));
+
+
+
+
