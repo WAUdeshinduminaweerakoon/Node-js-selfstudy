e18901974850186c77b9ab3510f78aa538646a3b
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-08-16 11:54:56 +0530

    while-do-while loop

---
+++ test/while-do-while-loop.js
@@ -0,0 +1,19 @@
+let i=0;
+// while(i<10){
+//     console.log("Hello World" , i);
+//     i++;
+// }
+/*Hello World 0
+Hello World 1
+Hello World 2
+Hello World 3
+Hello World 4
+Hello World 5
+Hello World 6
+Hello World 7
+Hello World 8
+Hello World 9*/
+do{
+    console.log("Hello World" , i);
+    i++;
+}while(i<10);
