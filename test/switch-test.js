c82c43b994abe7dbe592fe4b6ad747f90566eb8f
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-08-16 10:53:56 +0530

    Switch statement and ternary operator

---
+++ test/switch-test.js
@@ -0,0 +1,20 @@
+const result = "B";
+switch(result){
+    case "A":
+        console.log("Excellent");
+        break;
+    case "B":
+        console.log("Very Good");
+        break;
+    case "C":
+        console.log("Good");
+        break;
+     case "S":
+        console.log("Pass");
+        break;
+    case "W":
+        console.log("Fail");
+        break;
+    default:
+        console.log("Input is not valid");  
+}
