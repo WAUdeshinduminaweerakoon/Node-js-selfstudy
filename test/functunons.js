8eb795c208f7b873bac27d27eea1254d2a95be48
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-08-16 17:55:27 +0530

    functunons

---
+++ test/functunons.js
@@ -0,0 +1,35 @@
+
+// function hello(){
+//     console.log("Hello");
+//     //
+// }
+// hello();
+// hello();
+
+// function squre(num){
+//     return num*num;
+// }
+// const x = squre(4);
+// console.log(x);
+// console.log(squre(6));
+// console.log(squre(10));
+//---------------------------------------------------
+
+///----es6-----------
+// const squre = (x)=>{
+//     return x*x;
+// };
+// console.log(squre(7));
+
+// const squre1= (x)=>x*x;
+// console.log(squre1(8));
+
+// const squre1= x=>x*x;
+// console.log(squre1(9));
+
+const a = [1,2,3,4,5,6];
+
+// a.forEach(x=>console.log(x));
+
+const b = a.map(x=>x*2);
+console.log(b);
