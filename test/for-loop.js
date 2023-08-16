c669e137dd0fd41e52d4d720f1a3ef9ab7a1ec18
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-08-16 11:34:04 +0530

    for loop

---
+++ test/for-loop.js
@@ -0,0 +1,46 @@
+// for(let i=0;i<10;i++){
+//     console.log("Hello world");
+// }
+/*Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world*/
+// for(let i=0;i<10;i++){
+//     if(i==6){
+//         break;
+//     }
+//     console.log("Hello world", i);
+// }
+/*Hello world 0
+Hello world 1
+Hello world 2
+Hello world 3
+Hello world 4
+Hello world 5*/
+
+// for(let i=0;i<10;i++){
+//         if(i==6){
+//             continue;
+//         }
+//         console.log("Hello world", i);
+//     }
+    /*Hello world 0
+Hello world 1
+Hello world 2
+Hello world 3
+Hello world 4
+Hello world 5
+Hello world 7
+Hello world 8
+Hello world 9*/
+
+const x = "Hello";
+for(let i of x){
+    console.log(i);
+}
